619de5ad2b225b21e3e9a3db14d9dfe03f51550d
HieuTuan <tuannhse182788@fpt.edu.vn>
2026-01-21 10:11:14 +0700

    first commit

---
+++ src/main/java/com/mypkga/commerceplatformfull/CommercePlatformFullApplication.java
@@ -0,0 +1,13 @@
+package com.mypkga.commerceplatformfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePlatformFullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercePlatformFullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mypkga/commerceplatformfull/CommercePlatformFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypkga.commerceplatformfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercePlatformFullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
